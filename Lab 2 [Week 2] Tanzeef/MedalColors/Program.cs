8e345731e3672454fa6aa6f10bdbaa6e74257a63
Tanzeef <tanzeefahamed@gmail.com>
2025-11-06 12:44:29 -0500

    Add files via upload

---
+++ Lab 2 [Week 2] Tanzeef/MedalColors/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Drawing;
+
+namespace MedalColors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Medal m1 = new Medal("Horace Gwynne", "Boxing", MedalColor.Gold, 2012, true);
+            Medal m2 = new Medal("Michael Phelps", "Swimming", MedalColor.Gold, 2012, false);
+
+            List<Medal> medals = new List<Medal>() { m1, m2 };
+
+            medals.Add(new Medal("Ryan Cochrane", "Swimming", MedalColor.Silver, 2012, false));
+            medals.Add(new Medal("Adam van Koeverden", "Canoeing", MedalColor.Silver, 2012, false));
+            medals.Add(new Medal("Rosie MacLennan", "Gymnastics", MedalColor.Gold, 2012, false));
+            medals.Add(new Medal("Christine Girard", "Weightlifting", MedalColor.Bronze, 2012, false));
+            medals.Add(new Medal("Charles Hamelin", "Short Track", MedalColor.Gold, 2014, true));
+            medals.Add(new Medal("Alexandre Bilodeau", "Freestyle skiing", MedalColor.Gold, 2012, true));
+            medals.Add(new Medal("Jennifer Jones", "Curling", MedalColor.Gold, 2014, false));
+            medals.Add(new Medal("Charle Cournoyer", "Short Track", MedalColor.Bronze, 2014, false));
+            medals.Add(new Medal("Mark McMorris", "Snowboarding", MedalColor.Bronze, 2014, false));
+            medals.Add(new Medal("Sidney Crosby", "Ice Hockey", MedalColor.Gold, 2014, false));
+            medals.Add(new Medal("Brad Jacobs", "Curling", MedalColor.Gold, 2014, false));
+            medals.Add(new Medal("Ryan Fry", "Curling", MedalColor.Gold, 2014, false));
+            medals.Add(new Medal("Antoine Valois-Fortier", "Judo", MedalColor.Bronze, 2012, false));
+            medals.Add(new Medal("Brent Hayden", "Swimming", MedalColor.Bronze, 2012, false));
+
+           
+            Console.WriteLine("All of the 16 medals");
+            int count = 1;
+            foreach (Medal medal in medals)
+            {
+                Console.WriteLine($"{count}. {medal}");
+                count++;
+            }
+
+            Console.WriteLine("\nAll the names for 16 medals");
+            count = 1;
+            foreach (Medal medal in medals)
+            {
+                Console.WriteLine($"{count}. {medal.Name}");
+                count++;
+            }
+
+            Console.WriteLine("\nAll the names for 9 gold medals");
+            count = 1;
+            foreach (Medal medal in medals)
+            {
+                if (medal.Color == MedalColor.Gold)
+                {
+                    Console.WriteLine($"{count}. {medal.Name}");
+                    count++;
+                }
+            }
+
+           
+            Console.WriteLine("\nAll the names for 9 medals in 2012");
+            count = 1;
+            foreach (Medal medal in medals)
+            {
+                if (medal.Year == 2012)
+                {
+                    Console.WriteLine($"{count}. {medal.Name}");
+                    count++;
+                }
+            }
+
+            Console.WriteLine("\nAll the names for 4 medals in 2012");
+            count = 1;
+            foreach (Medal medal in medals)
+            {
+                if (medal.Year == 2012 && medal.Color == MedalColor.Gold)
+                {
+                    Console.WriteLine($"{count}. {medal.Name}");
+                    count++;
+                }
+            }
+
+            
+            Console.WriteLine("\nAll the names for 3 world record medals");
+            count = 1;
+            foreach (Medal medal in medals)
+            {
+                if (medal.IsRecord)
+                {
+                    Console.WriteLine($"{count}. {medal.Name}");
+                    count++;
+                }
+            }
+
+            
+            Console.WriteLine("\nSaving all the medals to file Medals.txt");
+            StreamWriter writer = new StreamWriter("Medals.txt");
+            foreach (Medal medal in medals)
+            {
+                writer.WriteLine(medal);
+            }
+            Console.ReadKey();
+        }
+
+        enum MedalColor
+        {
+            Gold,
+            Silver,
+            Bronze
+        }
+
+
+        class Medal
+        {
+            public string Name { get; }
+            public string TheEvent { get; }
+            public MedalColor Color { get; }
+            public int Year { get; }
+            public bool IsRecord { get; }
+
+            public Medal(string name, string theEvent, MedalColor color, int year, bool isRecord)
+            {
+                Name = name;
+                TheEvent = theEvent;
+                Color = color;
+                Year = year;
+                isRecord = isRecord;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name}, {TheEvent}, {Color}, {Year}, {IsRecord}";
+            }
+
+        }
+    }
+}
---
+++ Lab 2 [Week 2] Tanzeef/PetList/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace PetList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pets> pets = new List<Pets>();
+
+            Pets pet1 = new Pets("Fluffy", 4, "Persian Cat", "Micheal");
+            Pets pet2 = new Pets("Spike", 2, "Bulldog", "Micheal");
+            Pets pet3 = new Pets("Mittens", 1, "Siamese Cat", "Lexi");
+            Pets pet4 = new Pets("Buddy", 3, "Golden Retriever", "Lexi");
+
+            pets.Add(pet1);
+            pets.Add(pet2);
+            pets.Add(pet3);
+            pets.Add(pet4);
+
+            Console.WriteLine("\nThe Owner Micheal Owns: Persian Cat (Fluffy), Bulldog (Spike) \nThe Owner Lexi Owns: Golden Retriever (Buddy), Siamese Cat (Mittens)");
+
+            Console.WriteLine("\nWhat is the name of the owner of the pets?");
+            string owner = Console.ReadLine();
+            bool ownerFound = false;
+
+            foreach (Pets pet in pets)
+            {
+                if (pet.Owner == owner)
+                {
+                    Console.WriteLine($"Name: {pet.Name}, Age: {pet.Age}, Description: {pet.Description} ");
+                    ownerFound = true;
+                }
+            }
+
+            if (!ownerFound)
+            {
+                Console.WriteLine("The owner does not own any pets");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Pets
+    {
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+        public bool IsHouseTrained { get; set; }
+
+        public Pets(string name, int age, string description, string owner)
+        {
+            Name = name;
+            Age = age;
+            Description = description;
+            Owner = owner;
+        }
+
+        public void Train()
+        {
+            IsHouseTrained = true;
+        }
+        public void SetOwner(string owner)
+        {
+            Owner = owner;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Owner}, {Age}, {Description}, {IsHouseTrained}";
+        }
+    }
+}
